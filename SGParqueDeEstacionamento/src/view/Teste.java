e7d9e4d29b11a345485b013ba9f1ea76c88f5cd2
Arseniojc <122185340+Arseniojc@users.noreply.github.com>
2023-09-30 14:42:45 +0300

    Redimensionamento das telas

--- SGParqueDeEstacionamento/src/view/Teste.java
+++ SGParqueDeEstacionamento/src/view/Teste.java
@@ -8,16 +8,23 @@ public class Teste {
     public static void main(String[] args) {
         
         Veiculo v = new Veiculo();
+        Veiculo v1 = new Veiculo();
         
-        v.setMarca("Toyota");
-        v.setCor("Branco");
-        v.setMatricula("AM123");
-        v.setModelo("Corolla");
-        v.setTipo_veiculo("Ligeiro");
+      //  v.setMarca("Toyota");
+      //  v.setCor("Branco");
+      //  v.setMatricula("AM123");
+      //  v.setModelo("Corolla");
+      //  v.setTipo_veiculo("Ligeiro");
         
+        v1.setMarca("Ford");
+        v1.setCor("Preto");
+        v1.setModelo("Raptor");
+        v1.setMatricula("AB123");
+        v1.setTipo_veiculo("Ligeiro");
         
         VeiculoDAO dao = new VeiculoDAO();
         
-        v = dao.salvarVeiculo(v);
+      //  v = dao.salvarVeiculo(v);
+        v1 = dao.salvarVeiculo(v1);
     }
 }
